ee570738033f2dbce080ff9335d8534985d2017c
vegirevathi <vegirevathi@gmail.com>
2020-09-23 13:04:40 +0530

    ADDED: Centimetre enum constant in quantity measurement

--- main/controller/QuantityMeasurement.h
+++ main/controller/QuantityMeasurement.h
@@ -4,11 +4,12 @@ class QuantityMeasurement
 {
     public:
 
-    enum class QuantityBaseValue {INCH, FEET, YARD};
+    enum class QuantityBaseValue {INCH, FEET, YARD, CENTIMETRE};
 
     double INCH = 1;
     double FEET = 12;
     double YARD = 36;
+    double CENTIMETRE = 0.4;
 
     double convertToBase(double, double);
 };
